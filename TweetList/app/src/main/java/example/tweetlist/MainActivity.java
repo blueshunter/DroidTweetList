3e26d6d88c9ab4597d569d3ed9eb15482920c7dc
thoretton <thoretton.edwin@gmail.com>
2017-06-23 16:41:05 +0200

    refactor MainActivity

--- TweetList/app/src/main/java/example/tweetlist/MainActivity.java
+++ TweetList/app/src/main/java/example/tweetlist/MainActivity.java
@@ -4,70 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.Callback;
-import com.twitter.sdk.android.core.Result;
-import com.twitter.sdk.android.core.TwitterApiClient;
-import com.twitter.sdk.android.core.TwitterCore;
-import com.twitter.sdk.android.core.TwitterException;
-import com.twitter.sdk.android.core.models.Search;
-import com.twitter.sdk.android.core.models.Tweet;
-import com.twitter.sdk.android.core.services.SearchService;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import example.manager.TwitterManager;
-import retrofit2.Call;
-
+import example.helpers.ReachabilityHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText themeEditText;
-    private String themeText;
+    //constants
+    public static String EXTRA_MESSAGE = "com.example.activities.MainActivity.MESSAGE";
+
+    //view groups
+    private EditText mThemeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.initUI();
+        mThemeEditText = (EditText) this.findViewById(R.id.editText);
     }
 
-    private void initUI(){
+    /**
+     * called when user tap search button
+     */
+    public void searchAction(View v) {
 
-        themeEditText = (EditText) this.findViewById(R.id.editText);
-    }
+        // get edited text keywords
+        String mThemeText = mThemeEditText.getText().toString();
 
-    public void showTweets(List<Tweet> tweets){
+        //check reachability before launch intent
+        if(ReachabilityHelper.isOnline(getApplicationContext())){
 
-        Intent intent = new Intent(this, TweetListActivity.class);
-
-        List<String> tweetsIds = new ArrayList<>();
-        for(Tweet  tweet : tweets){
-            tweetsIds.add(tweet.idStr);
+            //create intent to launch new activity with extra message
+            Intent intent = new Intent(this, TweetListActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, mThemeText);
+            startActivity(intent);
         }
 
 
-
-        intent.putExtra("tweets", (Serializable)tweetsIds);
-        startActivity(intent);
-    }
-
-    public void searchAction(View v){
-
-
-        themeText = themeEditText.getText().toString();
-
-        //put theme on tweet list activity
-
-        //TODO check reachability before launch intent
-        Intent intent = new Intent(this, TweetListActivity.class);
-        intent.putExtra("search", themeText);
-        startActivity(intent);
     }
 }
